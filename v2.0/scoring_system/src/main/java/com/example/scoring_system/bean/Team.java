f3ff37bf8914e30929148c14fc0ab1811a755ece
郑龙海 <79756877+Hector2333@users.noreply.github.com>
2021-05-11 17:04:20 +0800

    团队类

--- v2.0/scoring_system/src/main/java/com/example/scoring_system/bean/Team.java
+++ v2.0/scoring_system/src/main/java/com/example/scoring_system/bean/Team.java
@@ -1,18 +1,31 @@
 package com.example.scoring_system.bean;
 
+import cn.afterturn.easypoi.excel.annotation.Excel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.List;
+import java.io.Serializable;
 
 @Data
-public class Team {
+public class Team implements Serializable {
     private String id;
-    private String sysTeamName;
-    private String sysTeamSlogan;
-    private String classRoomId;
-    private ClassRoom classRoom;
-    private List<BlogWork> blogWorkList;
-    private List<User> userList;
 
+    @Excel(name="团队名",orderNum="1")
+    private String teamName;
 
+    @Excel(name="团队口号",orderNum="2")
+    private String teamSlogan;
+
+    @Excel(name="所属班级号",orderNum="3")
+    private String classId;
+
+    public Team(){
+
+    }
+
+    public Team(String b,String c,String d){
+        this.teamName = b;
+        this.teamSlogan = c;
+        this.classId = d;
+    }
 }
