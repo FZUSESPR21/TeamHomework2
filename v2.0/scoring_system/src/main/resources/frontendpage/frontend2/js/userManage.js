b8080f8d04176427700f652713fcb1a275cead57
LiJming <81281497+LiJming@users.noreply.github.com>
2021-06-10 22:19:36 +0800

    初步整合

---
+++ v2.0/scoring_system/src/main/resources/frontendpage/frontend2/js/userManage.js
@@ -0,0 +1,3 @@
+document.getElementById("content-manage").style.display="none";
+document.getElementById("user-manage").style.display="block";
+document.getElementById("statistical-analysis").style.display="none";
